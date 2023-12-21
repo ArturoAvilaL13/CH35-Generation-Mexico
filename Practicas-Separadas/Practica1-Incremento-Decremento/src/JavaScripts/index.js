7448e937805ad08ba1c72c86369a52e3cfa2c6cb
ArturoAvilaL13 <arturo.avila1319@gmail.com>
2023-12-20 20:39:42 -0600

    Practica1 con la opciones de setInterval

--- Practicas-Separadas/Practica1-Incremento-Decremento/src/JavaScripts/index.js
+++ Practicas-Separadas/Practica1-Incremento-Decremento/src/JavaScripts/index.js
@@ -5,14 +5,24 @@ const reset = document.getElementById("reset");
 const maximo = 100;
 const minimo = -100;
 const inicial = 0;
+let intervalo = null;
 contador.innerText = inicial;
 incrementar.addEventListener("click", () => {
-  if (Number(contador.textContent) < 100) iniciarContador("+");
+  if (Number(contador.textContent) < 100) {
+    //iniciarContador("+");
+    iniciarContadorInterval("+");
+  }
 });
 decrementar.addEventListener("click", () => {
-  if (Number(contador.textContent) > -100) iniciarContador("-");
+  if (Number(contador.textContent) > -100) {
+    // iniciarContador("-");
+    iniciarContadorInterval("-");
+  }
 });
-reset.addEventListener("click", () => iniciarContador("s"));
+reset.addEventListener("click", () => {
+  // iniciarContador("s");
+  iniciarContadorInterval("s");
+});
 
 const iniciarContador = (orden) => {
   switch (orden) {
@@ -24,6 +34,37 @@ const iniciarContador = (orden) => {
       break;
     case "-":
       contador.innerText = Number(contador.textContent) - 1;
+      break;
+    default:
+      break;
+  }
+};
+
+const iniciarContadorInterval = (orden) => {
+  switch (orden) {
+    case "s":
+      clearInterval(intervalo);
+      contador.innerText = 0;
+      break;
+    case "+":
+      if (intervalo != null) clearInterval(intervalo);
+      intervalo = setInterval(() => {
+        if (Number(contador.textContent) < 100) {
+          contador.innerText = Number(contador.textContent) + 1;
+        } else {
+          clearInterval(intervalo);
+        }
+      }, 500);
+      break;
+    case "-":
+      if (intervalo != null) clearInterval(intervalo);
+      intervalo = setInterval(() => {
+        if (Number(contador.textContent) > -100) {
+          contador.innerText = Number(contador.textContent) - 1;
+        } else {
+          clearInterval(intervalo);
+        }
+      }, 500);
       break;
     default:
       break;
